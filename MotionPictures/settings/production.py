9b390d49eddbe01f8c109a92d5ac586cdc28b4b5
ajay <ajaymundhe21@gmail.com>
2017-12-10 18:37:30 +0530

    Production For Media_URL change

--- MotionPictures/settings/production.py
+++ MotionPictures/settings/production.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 #STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 #MEDIA_ROOT =  os.path.join((BASE_DIR),"mediafiles", )
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
