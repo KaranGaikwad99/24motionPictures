f21eecbafb42425e8664ce9245c4fce12a1eaf7b
ajay <ajaymundhe21@gmail.com>
2017-12-11 16:34:15 +0530

    Images deleted

--- MotionPictures/settings/production.py
+++ MotionPictures/settings/production.py
@@ -174,7 +174,7 @@ AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL ='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
