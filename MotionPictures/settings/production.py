ae6ba2fd052b641f583f9546b3bf75a0d65d098c
ajay <ajaymundhe21@gmail.com>
2017-12-11 15:49:53 +0530

    change

--- MotionPictures/settings/production.py
+++ MotionPictures/settings/production.py
@@ -182,4 +182,4 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
-DEFAULT_FILE_STORAGE='mediafiles.storage_backends.MediaStore'
+DEFAULT_FILE_STORAGE='MotionPictures.storage_backends.MediaStore'
