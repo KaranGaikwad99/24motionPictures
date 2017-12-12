5e4d5e96058e4bb478642e4e5a2b071cb39966cd
ajay <ajaymundhe21@gmail.com>
2017-12-12 15:17:18 +0530

    modified models of photography

--- photography/models.py
+++ photography/models.py
@@ -4,6 +4,7 @@ import uuid
 from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
+from MotionPictures.settings.aws import utils 
 
 class Album(models.Model):
     title = models.CharField(max_length=70)
@@ -22,7 +23,7 @@ class Album(models.Model):
         return self.title
 
 class AlbumImage(models.Model):
-    image = ProcessedImageField(upload_to='albums', processors=[ResizeToFit(1280)], format='JPEG', options={'quality': 1024})
+    image = ProcessedImageField(upload_to='MediaRootS3BotoStorage',)
     thumb = ProcessedImageField(upload_to='albums', processors=[ResizeToFit(300)], format='JPEG', options={'quality': 80})
     album = models.ForeignKey('album')
     alt = models.CharField(max_length=255, default=uuid.uuid4)
