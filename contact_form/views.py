a198cef6a78009c771e8370553d744709d2e3161
ajay <ajaymundhe21@gmail.com>
2017-12-12 16:53:52 +0530

    form edited

--- contact_form/views.py
+++ contact_form/views.py
@@ -4,30 +4,48 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from contact_form.forms import ContactForm
 from django.views.generic.base import TemplateView
-from MotionPictures.settings import base
+from django.template.loader import get_template
+from MotionPictures.settings import production
 # Create your views here.
 
 def contact(request):
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-        save_it=form.save(commit=False)
-        save_it.save()
-        Name=form.cleaned_data['first_name']
-        Surname=form.cleaned_data['last_name']
-        CHOICES = form.cleaned_data['CHOICES']
-        email = form.cleaned_data['email']
-        message = form.cleaned_data['message']
-        subject= 'site contact form'
-        from_email= base.EMAIL_HOST_USER
-        to_email=[save_it.email,'ajaymundhe21@gmail.com'] 
-        send_mail(subject,
-                message, 
-                from_email, 
-                to_email,
-                fail_silently=False)
-    context = {
+    if request.method == 'POST':
+        form=form_class(data=request.POST)
+        if form.is_valid():
+            Name=request.POST.get('first_name','')
+            Surname=request.POST.get('last_name','')
+            CHOICES=request.POST.get('CHOICES','')
+            email=request.POST.get('email','')
+            message=request.POST.get('message','')
+            template = get_template('contact_template.txt')
+            context = {
             "form":form,
-    }
+                }
+            content=template.render(context)
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "24MotionPictures" +'',
+                ['ajaymundhe21@gmail.com'],
+                headers = {'Reply-To': contact_email }
+            )
+            email.send()
+            return redirect('contact')
     return render(request, "contact.html",context)
+
+
+
+
+
+
+      #  subject= 'site contact form'
+       # from_email= base.EMAIL_HOST_USER
+        #to_email=[save_it.email,'ajaymundhe21@gmail.com'] 
+        #send_mail(subject,
+         #       message, 
+          #      from_email, 
+           #     to_email,
+            #    fail_silently=False)
+
 
 
