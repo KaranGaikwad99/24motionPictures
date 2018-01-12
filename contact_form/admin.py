52d8a0a78cd6c76419f9fb262fb9c0e2adb6ebbf
ajay <ajaymundhe21@gmail.com>
2018-01-12 13:52:28 +0530

    contact

--- contact_form/admin.py
+++ contact_form/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserData
+
+
+class UserDataModelAdmin(admin.ModelAdmin):
+	list_display=["__unicode__","name","surname","choice","email","message"]
+	class Meta:
+		model=UserData
+
+
+admin.site.register(UserData, UserDataModelAdmin)
