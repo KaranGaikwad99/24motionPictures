27e3216fc196f216c00065993184c2b56d8da9f2
ajay <ajaymundhe21@gmail.com>
2018-01-15 14:13:58 +0530

    site

--- MotionPictures/settings/production.py
+++ MotionPictures/settings/production.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'django.contrib.sites',
+   
     'crispy_forms',
     'home',
     'photography',
